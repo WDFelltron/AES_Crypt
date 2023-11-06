997a597e0a012d0cab1323997fb14f060aefa737
k.lapuzin@outlook.com <k.lapuzin@outlook.com>
2023-11-06 13:24:31 +0500

    Есть интерфейс и код, осталось устранить ошибку чтения ключа

--- AES_Crypt/AES.cs
+++ AES_Crypt/AES.cs
@@ -3,11 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace AES_Crypt
 {
     internal class AES
     {
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("0123456789ABCDEF"); // 16-byte key
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("1234567890ABCDEF"); // 16-byte initialization vector
 
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] encryptedBytes = null;
+
+                using (var ms = new System.IO.MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var sw = new System.IO.StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+
+                        encryptedBytes = ms.ToArray();
+                    }
+                }
+
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                string decryptedText = null;
+
+                using (var ms = new System.IO.MemoryStream(cipherTextBytes))
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var sr = new System.IO.StreamReader(cs))
+                        {
+                            decryptedText = sr.ReadToEnd();
+                        }
+                    }
+                }
+
+                return decryptedText;
+            }
+        }
     }
 }
--- AES_Crypt/MainWindow.xaml.cs
+++ AES_Crypt/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,75 @@ namespace AES_Crypt
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private string EncryptText(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                byte[] key = Encoding.UTF8.GetBytes("YourEncryptionKey");
+                byte[] iv = Encoding.UTF8.GetBytes("YourEncryptionIV");
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] encryptedBytes;
+                using (var ms = new System.IO.MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                        cs.Write(plainBytes, 0, plainBytes.Length);
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+
+                string encryptedText = Convert.ToBase64String(encryptedBytes);
+                return encryptedText;
+            }
+        }
+
+        private string DecryptText(string encryptedText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                byte[] key = Encoding.UTF8.GetBytes("YourEncryptionKey");
+                byte[] iv = Encoding.UTF8.GetBytes("YourEncryptionIV");
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+                byte[] decryptedBytes;
+                using (var ms = new System.IO.MemoryStream(encryptedBytes))
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var sr = new System.IO.StreamReader(cs))
+                        {
+                            string decryptedText = sr.ReadToEnd();
+                            return decryptedText;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            string plainText = txtPlainText.Text;
+            string encryptedText = EncryptText(plainText);
+            txtEncryptedText.Text = encryptedText;
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            string encryptedText = txtEncryptedText.Text;
+            string decryptedText = DecryptText(encryptedText);
+            txtDecryptedText.Text = decryptedText;
         }
     }
 }
