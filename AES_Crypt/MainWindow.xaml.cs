c7f8d310a67ff6e168c2bde12989221a41d98c97
k.lapuzin@outlook.com <k.lapuzin@outlook.com>
2023-11-06 14:24:51 +0500

    Доработал простую программу на шифрование и дешифрование с алгоритмом AES

--- AES_Crypt/MainWindow.xaml.cs
+++ AES_Crypt/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -21,77 +22,71 @@ namespace AES_Crypt
     /// </summary>
     public partial class MainWindow : Window
     {
+        byte[] key = new byte[16];
+
+        byte[] iv = new byte[16];
         public MainWindow()
         {
             InitializeComponent();
-        }
-        private string EncryptText(string plainText)
-        {
-            using (Aes aes = Aes.Create())
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
             {
-                byte[] key = Encoding.UTF8.GetBytes("YourEncryptionKey");
-                byte[] iv = Encoding.UTF8.GetBytes("YourEncryptionIV");
-
-                aes.Key = key;
-                aes.IV = iv;
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                byte[] encryptedBytes;
-                using (var ms = new System.IO.MemoryStream())
-                {
-                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
-                        cs.Write(plainBytes, 0, plainBytes.Length);
-                    }
-                    encryptedBytes = ms.ToArray();
-                }
-
-                string encryptedText = Convert.ToBase64String(encryptedBytes);
-                return encryptedText;
+                rng.GetBytes(key);
+                rng.GetBytes(iv);
             }
         }
-
-        private string DecryptText(string encryptedText)
+        static byte[] EncryptText(string simpletext, byte[] key, byte[] iv)
         {
+            byte[] cipheredtext;
             using (Aes aes = Aes.Create())
             {
-                byte[] key = Encoding.UTF8.GetBytes("YourEncryptionKey");
-                byte[] iv = Encoding.UTF8.GetBytes("YourEncryptionIV");
-
-                aes.Key = key;
-                aes.IV = iv;
-
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
-                byte[] decryptedBytes;
-                using (var ms = new System.IO.MemoryStream(encryptedBytes))
+                ICryptoTransform encryptor = aes.CreateEncryptor(key, iv);
+                using (MemoryStream memoryStream = new MemoryStream())
                 {
-                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
                     {
-                        using (var sr = new System.IO.StreamReader(cs))
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
                         {
-                            string decryptedText = sr.ReadToEnd();
-                            return decryptedText;
+                            streamWriter.Write(simpletext);
+                        }
+
+                        cipheredtext = memoryStream.ToArray();
+                    }
+                }
+            }
+            return cipheredtext;
+        }
+        static string DecryptText(byte[] cipheredtext, byte[] key, byte[] iv)
+        {
+            string simpletext = String.Empty;
+            using (Aes aes = Aes.Create())
+            {
+                ICryptoTransform decryptor = aes.CreateDecryptor(key, iv);
+                using (MemoryStream memoryStream = new MemoryStream(cipheredtext))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            simpletext = streamReader.ReadToEnd();
                         }
                     }
                 }
             }
+            return simpletext;
         }
+
 
         private void Encrypt_Click(object sender, RoutedEventArgs e)
         {
             string plainText = txtPlainText.Text;
-            string encryptedText = EncryptText(plainText);
-            txtEncryptedText.Text = encryptedText;
+            byte[] encryptedText = EncryptText(plainText, key, iv);
+            txtEncryptedText.Text = Convert.ToBase64String(encryptedText);
         }
 
         private void Decrypt_Click(object sender, RoutedEventArgs e)
         {
-            string encryptedText = txtEncryptedText.Text;
-            string decryptedText = DecryptText(encryptedText);
+            byte[] encryptedText = Convert.FromBase64String(txtEncryptedText.Text);
+            string decryptedText = DecryptText(encryptedText, key, iv);
             txtDecryptedText.Text = decryptedText;
         }
     }
